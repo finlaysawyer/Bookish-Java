59eb5c4cbae2009520a523ec441fc8c3a6fea358
Finlay <finjsawyer@gmail.com>
2020-10-28 11:42:25 +0000

    Retrieve books

---
+++ src/main/java/org/softwire/training/bookish/models/database/Loan.java
@@ -0,0 +1,10 @@
+package org.softwire.training.bookish.models.database;
+
+import java.text.SimpleDateFormat;
+
+public class Loan {
+    private int loan_id;
+    private SimpleDateFormat return_date;
+    private int member_id;
+    private int book_id;
+}
